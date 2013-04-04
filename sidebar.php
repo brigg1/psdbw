e9ace40686194fcf7cb2da0a1a26ed03409f656d
Mike Macfadden <mikemacfadden@gmail.com>
2013-04-04 12:04:37 -0500

    initial commit

---
+++ sidebar.php
@@ -0,0 +1,17 @@
+<div id="sidebar">
+  <a href="http://mmacfadden.com">
+    <img src="images/mmacfadden.png" id="mmacfadden"/>
+  </a>
+  <h3>How does </br>PSDBW work?</h3>
+  <ol>
+    <li>Write and style your daily bulletin using the WYSIWYG to the right.</li>
+    <li>Click the "HTML" button to view and copy the generated "HTML" code.</li>
+    <li>Paste the code in the text area in PowerSchool and click submit.</li>
+  </ol>
+
+  <a href="javascript:alert('coming soon...');"><img src="images/play.jpg" id="play"/></a>
+
+  <p><a href="http://mmacfadden.com">Cobbled together by <br/>Mike Macfadden</a></p>
+
+</div>
+
